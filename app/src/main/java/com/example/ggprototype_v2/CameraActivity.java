609c90b4c254a99b352c9cec25f6831eda7171eb
grilledtoast3r <mckevitt-m2@ulster.ac.uk>
2020-02-20 18:08:48 +0000

    Augmented Image Database Changes

--- app/src/main/java/com/example/ggprototype_v2/CameraActivity.java
+++ app/src/main/java/com/example/ggprototype_v2/CameraActivity.java
@@ -4,6 +4,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.ar.core.Anchor;
@@ -18,12 +22,16 @@ import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
+import java.util.logging.Logger;
 
 
 public class CameraActivity extends AppCompatActivity implements Scene.OnUpdateListener{
 
     private CustomArFragment arFragment;
+    private TextView textView;
 
 
     @Override
@@ -32,15 +40,31 @@ public class CameraActivity extends AppCompatActivity implements Scene.OnUpdateL
         setContentView(R.layout.activity_camera);
 
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
-        arFragment.getArSceneView().getScene().addOnUpdateListener(this);
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdate);
+
+        textView = findViewById(R.id.trackingStatus);
 
     }
 
+    /***
+     * Establishing an Augmented Image Database for use with AR CORE
+     * @param config see ArCore Documentation for details
+     * @param session see ArCore Documentation for details
+     *      First this method creates All images as objects of type
+     *      Bitmap from the android.graphics package.
+     *
+     *      Then the method adds those Images to the augmented image database
+     */
     public  void setUpDatabase(Config config, Session session) {
-        Bitmap brotherBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brother);
-        AugmentedImageDatabase aid = new AugmentedImageDatabase(session);
-        aid.addImage("brother", brotherBitmap);
-        config.setAugmentedImageDatabase(aid);
+
+        InputStream dbStream = getResources().openRawResource(R.raw.gg);
+
+        try {
+            AugmentedImageDatabase aid = AugmentedImageDatabase.deserialize(session,dbStream);
+            config.setAugmentedImageDatabase(aid);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -50,21 +74,57 @@ public class CameraActivity extends AppCompatActivity implements Scene.OnUpdateL
         Collection<AugmentedImage> images = frame.getUpdatedTrackables(AugmentedImage.class);
 
         for (AugmentedImage image : images){
-            if (image.getTrackingState() == TrackingState.TRACKING) {
-                if (image.getName().equals("brother")) {
-                    Anchor anchor = image.createAnchor(image.getCenterPose());
+            if (image.getTrackingMethod() == AugmentedImage.TrackingMethod.FULL_TRACKING) {
+                Anchor anchor = image.createAnchor(image.getCenterPose());
+                switch (image.getName()){
 
-                    createModel(anchor);
+                    case "brother.jpg":
+                        createModel(anchor, "beer");
+                        textView.setText("brother is visible");
+                        break;
+                    case "bb.jpg":
+                        textView.setText("bb is visible");
+                        break;
+                    case "cat.jpg":
+                        createModel(anchor, "cat");
+                        textView.setText("cat is visible");
+                        break;
+                    case "dog.jpg":
+                        createModel(anchor, "dog");
+                        textView.setText("dog is visible");
+                        break;
+                    case "flowers.jpg":
+                        createModel(anchor, "flowers");
+                        textView.setText("flowers is visible");
+                        break;
                 }
+                /*if (image.getName().equals("brother")) {
+
+                }*/
             }
         }
     }
 
-    private void createModel(Anchor anchor) {
-        ModelRenderable.builder()
-                .setSource(this, Uri.parse("model.sfb"))
-                .build()
-                .thenAccept(modelRenderable -> placeModel(modelRenderable, anchor));
+    private void createModel(Anchor anchor, String modelName) {
+        String selection = modelName.toLowerCase();
+
+        switch (selection){
+            case "beer":
+                ModelRenderable.builder()
+                        .setSource(this, Uri.parse("model.sfb"))
+                        .build()
+                        .thenAccept(modelRenderable -> placeModel(modelRenderable, anchor));
+                break;
+
+            default:
+                Toast.makeText(this, "Error: no model was called", Toast.LENGTH_LONG).show();
+
+                System.out.println("Error: no model was called. Expected: lower case string mathcing a model.sfb file. Received: "+ selection  );
+        }
+
+
+
+
     }
 
     private void placeModel(ModelRenderable modelRenderable, Anchor anchor) {
