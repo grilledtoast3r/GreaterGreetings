fe3d25945c6ba0e229382fd8966c76d5c85d1351
grilledtoast3r <mckevitt-m2@ulster.ac.uk>
2020-03-09 10:30:48 +0000

    WIP Update for Reminder System

---
+++ app/src/main/java/com/example/ggprototype_v2/AddReminderActivity.java
@@ -0,0 +1,14 @@
+package com.example.ggprototype_v2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
--- app/src/main/java/com/example/ggprototype_v2/MainActivity.java
+++ app/src/main/java/com/example/ggprototype_v2/MainActivity.java
@@ -9,27 +9,45 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button cameraButton;
+    private Button notificationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.openCameraActivityButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        cameraButton = (Button) findViewById(R.id.openCameraActivityButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCameraActivity();
             }
         });
+
+        notificationButton = (Button) findViewById(R.id.openNotificationActivityButton);
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNotificationActivity();
+            }
+        });
+
     }
 
     /*
-    * Method for changing activity
+    * Method for changing to camera activity
     * */
     public void openCameraActivity(){
         Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    /*
+     * Method for changing to notification activity
+     * */
+    public void openNotificationActivity(){
+        Intent intent = new Intent(this, NotificationActivity.class);
         startActivity(intent);
     }
 }
